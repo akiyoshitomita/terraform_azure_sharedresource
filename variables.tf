1882cb9cff9e440232f22da73b57788c5b38f935
akiyoshitomita <akiyoshitomita@live.jp>
2019-11-25 05:37:22 +0000

    Keyvaul作成

--- variables.tf
+++ variables.tf
@@ -1,32 +1,32 @@
 
 variable "resource_group" {
   description = "リソースグループ名"
-  type        = "string"
+  type        = string
 }
 
 variable "location" {
   description = "Azureのロケーション名"
-  type        = "string"
+  type        = string
   default     = "japaneast"
 }
 
 variable "strage_diag_name"{
   description = "ダイアグ情報を保存するための共通ストレージアカウント名"
-  type        = "string"
+  type        = string
 }
 
 variable "tag_owner" {
   description = "利用者"
-  type        = "string"
+  type        = string
 }
 
 variable "tag_application" {
   description = "利用目的"
-  type        = "string"
+  type        = string
   default     = "テスト利用"
 }
 
 variable "tag_expiration" {
   description = "利用期日"
-  type        = "string"
+  type        = string
 }
